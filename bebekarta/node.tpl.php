908045eeb9ce5ba3980a41d5ed10c2be41232c92
Alexander Todorov <atodorov@otb.bg>
2011-10-06 22:05:09 +0300

    print node type and vip icons

--- bebekarta/node.tpl.php
+++ bebekarta/node.tpl.php
@@ -7,7 +7,15 @@
     }?>
 
     <?php if ($node->type != 'page') { ?>
-        <h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><br />
+        <h2 class="title">
+           <?php
+               if (($node->type == 'podariava') || ($node->type == 'prodava')) {
+                   printf("<img src='/sites/all/themes/bebekarta/images/%s.png' alt='' />&nbsp;", $node->type);
+                   printf("<img src='/sites/all/themes/bebekarta/images/vip_%d.png' alt='ВИП' />", $node->field_vip[0][value]);
+               }
+           ?>
+           <a href="<?php print $node_url?>"><?php print $title?></a>
+        </h2><br />
     <?php }; ?>
 
     <div class="content clear-block"><?php print $content?></div>
